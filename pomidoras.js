6885f5981a53200c235efa6110f574b7a5010c5b
Karolis <karolisdarbinis2@gmail.com>
2023-06-06 23:07:06 +0300

    Pomidoras

---
+++ pomidoras.js
@@ -0,0 +1,28 @@
+console.clear();
+
+let int1 = 10;
+let int2 = 20;
+
+if(int1 > int2){
+    console.log('As pomidoras');
+}else {
+    console.log('Tu pomidoras');
+}
+
+let stringas = [1, 2, 3, 4];
+let stringas2 = [1, 1, 1, 1, 1];
+
+if(stringas.length > stringas2.length){
+    console.log('As pomidoras');
+}else {
+    console.log('Tu pomidoras');
+}
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 1, 1, 1, 1];
+
+if(arr1.length > arr2.length){
+    console.log('As pomidoras');
+}else {
+    console.log('Tu pomidoras');
+}
