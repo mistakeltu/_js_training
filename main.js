11b4862b4b955e635ba5da3ff552759c701839c7
Karolis <karolisdarbinis2@gmail.com>
2023-06-19 17:04:34 +0300

    Update

--- main.js
+++ main.js
@@ -2023,3 +2023,47 @@ console.clear();
 // CODEWARS kodai:
 
 // const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+// function toCsvText(array) {
+//     let result = '';
+//     for(let i = 0; i < array.length; i++){
+//         result += array[i];
+//         if(i !== array.length - 1){
+//             result += '\n';
+//         }
+//     }
+//     return result;
+//  }
+
+//  console.log(toCsvText(
+//    [[ 0, 1, 2, 3, 4 ],
+//      [ 10,11,12,13,14 ],
+//      [ 20,21,22,23,24 ],
+//      [ 30,31,32,33,34 ]] 
+//  ));
+
+//  CODEWARS kodai:
+
+//  function toCsvText(array) {
+//     return array.join('\n');
+//  }
+
